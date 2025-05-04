d80b2cdfc98991ef77ff3c9e656f72bda6ab75f9
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-04 21:31:37 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiQ7ZevjZatISUTyDbRoXd695gfQGiksU",
+  authDomain: "event-explorar.firebaseapp.com",
+  projectId: "event-explorar",
+  storageBucket: "event-explorar.firebasestorage.app",
+  messagingSenderId: "182385174365",
+  appId: "1:182385174365:web:4042c6786b06ed87156ab4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
