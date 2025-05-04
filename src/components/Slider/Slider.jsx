e091cbf08dd2660d8712df7c97252a7bad4ace4f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-05 01:23:41 +0600

    updated slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
 
 const Slider = () => {
-    return (
-        <div>
-            slider
+  return (
+    <div className="carousel w-full">
+      {/* Slide 1 */}
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+          className="w-full"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide3" className="btn btn-circle">❮</a>
+          <a href="#slide2" className="btn btn-circle">❯</a>
         </div>
-    );
+      </div>
+
+      {/* Slide 2 */}
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+          className="w-full"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide1" className="btn btn-circle">❮</a>
+          <a href="#slide3" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+
+      {/* Slide 3 */}
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+          className="w-full"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide2" className="btn btn-circle">❮</a>
+          <a href="#slide1" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
