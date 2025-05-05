da1258f4a73944f68e35ac94474b89dc8094a7c5
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-05 15:28:34 +0600

    updated upcoming event and json data and added card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Card = ({card}) => {
+
+    
+
+    return (
+        <div className="bg-white rounded-2xl shadow-md overflow-hidden flex flex-col">
+    <img
+        src={card.thumbnail}
+        alt={card.name}
+        className="w-full h-48 object-cover"
+    />
+    <div className="p-4 flex flex-col flex-grow">
+        <h3 className="text-xl font-semibold">{card.name}</h3>
+        <p className="text-sm text-gray-600">{card.date} — {card.location}</p>
+        <p className="mt-2 text-gray-700 line-clamp-3">{card.description}</p>
+        <div className="mt-4 flex justify-between items-center">
+            <span className="text-sm text-blue-600 font-medium">{card.category}</span>
+            <span className="text-sm text-green-600 font-bold">${card.entry_fee}</span>
+        </div>
+        <button className="btn mt-8 w-full font-semibold py-2 px-4 rounded-xl ">
+            View More
+        </button>
+    </div>
+</div>
+    );
+};
+
+export default Card;
--- src/components/UpcomingEvents/UpcomingEvents.jsx
+++ src/components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
+import Card from '../Card/Card';
+
 
 const UpcomingEvents = () => {
+
+    const data = useLoaderData();
+    console.log(data);
+
+
     return (
-        <div>
-            upcoming
-        </div>
+        
+        <>
+            <h1 className="text-3xl font-bold mt-24 text-center">Upcoming Events</h1>
+            <p className='text-gray-600 mt-3 mb-9 text-center'>Discover the latest events happening near you. Stay informed and join the experiences that matter.</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {
+                data.map((card, index) => <Card key={index} card={card}></Card>)
+            }
+            </div>
+        </>
     );
 };
 
