13d49638e6b47f344de288e1bcf75804e0efebf6
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-06 22:32:38 +0600

    updated menu styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 -mr-2"
+              className="h-5 w-5 -mr-5 -ml-5"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
