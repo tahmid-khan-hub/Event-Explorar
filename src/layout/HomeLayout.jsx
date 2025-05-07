6bba13271c65c3e9582c6cde5d6d1432dd0f4f12
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-07 21:08:39 +0600

    updated footer and its icon link

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer.jsx/Footer';
+import Footer from '../components/Footer/Footer' 
 import { Outlet } from 'react-router';
 import { ToastContainer } from 'react-toastify';
 
